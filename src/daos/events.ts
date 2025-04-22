857b3e9492bc557fd98791858ea69f4e38fa2ee3
herby2212 <12448284+herby2212@users.noreply.github.com>
2025-04-22 09:39:12 +0200

    add seasons to events

--- src/daos/events.ts
+++ src/daos/events.ts
@@ -1,15 +1,22 @@
 import eventsData from '../../data/events.json';
+import { Season, Seasons } from './seasons';
 
 export class Event {
     constructor(
-        public readonly id: string
+        public readonly id: string,
+        public readonly seasons: Season[]
     ) {}
 
     public static loadEvents(): Record<string, Event> {
         const events: Record<string, Event> = {};
         for (const [key, value] of Object.entries(eventsData)) {
+            const seasons: Season[] = value.seasons.map((_season: string) => {
+                const season = _season as keyof typeof Seasons;
+                return Seasons[season];
+            });
             events[key] = new Event(
-                value.id
+                value.id,
+                seasons
             );
         }
         return events;
