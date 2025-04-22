7b1d1a842465ccd4ec7184acb3e952766efdbe7c
herby2212 <12448284+herby2212@users.noreply.github.com>
2025-04-22 09:18:04 +0200

    add worldEvents data and corresponding dao

---
+++ src/daos/worldEvents.ts
@@ -0,0 +1,23 @@
+import worldEventsData from '../../data/worldEvents.json';
+
+export class WorldEvent {
+    constructor(
+        public readonly id: string
+    ) {}
+
+    public static loadWorldEvents(): Record<string, WorldEvent> {
+        const worldEvents: Record<string, WorldEvent> = {};
+        for (const [key, value] of Object.entries(worldEventsData)) {
+            worldEvents[key] = new WorldEvent(
+                value.id
+            );
+        }
+        return worldEvents;
+    }
+}
+
+export type WorldEvents = {
+    [K in keyof typeof worldEventsData]: WorldEvent;
+};
+
+export const WorldEvents = WorldEvent.loadWorldEvents() as WorldEvents;
