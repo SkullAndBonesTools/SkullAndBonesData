44f0e33df0fe4e22d381dbf5cc9faa78969a09f4
herby2212 <12448284+herby2212@users.noreply.github.com>
2025-05-06 00:09:17 +0200

    add dao for modifications

---
+++ src/daos/modifications.ts
@@ -0,0 +1,40 @@
+import modificationsData from '../../data/modifications.json';
+import { EffectType, Grade } from '../types/ModificationProperties';
+
+export class Modification {
+    constructor(
+        public readonly id: string,
+        public readonly itemType: string[],
+        public readonly effectType: EffectType | undefined,
+        public readonly range: number[],
+        public readonly dropOnly: boolean,
+        public readonly repairOnly: boolean | undefined,
+        public readonly grade: Grade,
+    ) {}
+
+    public static fromRawData(rawData: any): Modification {
+        return new Modification(
+            rawData.id,
+            rawData.itemType,
+            rawData.effectType ?? undefined,
+            rawData.range,
+            rawData.dropOnly,
+            rawData.repairOnly ?? false,
+            rawData.grade as Grade,
+        );
+    }
+
+    public static loadModifications(): Record<string, Modification> {
+        const modifications: Record<string, Modification> = {};
+        for (const [key, value] of Object.entries(modificationsData)) {
+            modifications[key] = Modification.fromRawData(value);
+        }
+        return modifications;
+    }
+}
+
+type Modifications = {
+    [K in keyof typeof modificationsData]: Modification;
+};
+
+export const Modifications = Modification.loadModifications() as Modifications;
