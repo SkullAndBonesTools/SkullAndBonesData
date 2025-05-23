9430c597fa4e82a373a31e98f8b08507b314b79d
herby2212 <12448284+herby2212@users.noreply.github.com>
2025-05-23 09:54:22 +0200

    add event field to commodities

--- src/daos/commodities.ts
+++ src/daos/commodities.ts
@@ -1,22 +1,30 @@
 import commoditiesData from "../../data/commodities.json";
 import { CommodityCategory } from "../types/Category";
 import { Rarity } from "../types/Rarity";
+import { Event, Events } from "./events";
 
 export class Commodity {
     constructor(
         public readonly id: string,
         public readonly rarity: Rarity,
-        public readonly category: CommodityCategory
+        public readonly category: CommodityCategory,
+        public readonly event?: Event
     ) {}
+
+    public static fromRawData(rawData: any): Commodity {
+        const event = rawData.event as keyof typeof Events;
+        return new Commodity(
+            rawData.id,
+            rawData.rarity as Rarity,
+            rawData.category as CommodityCategory,
+            event ? Events[event] : undefined
+        );
+    }
 
     public static loadCommodities(): Record<string, Commodity> {
         const commodities: Record<string, Commodity> = {};
         for (const [key, value] of Object.entries(commoditiesData)) {
-            commodities[key] = new Commodity(
-                key,
-                value.rarity as Rarity,
-                value.category as CommodityCategory
-            );
+            commodities[key] = Commodity.fromRawData(value);
         }
         return commodities;
     }
