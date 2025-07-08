ffef18b6c9e97d61456ba7085fa8f7b39863b69d
herby2212 <12448284+herby2212@users.noreply.github.com>
2025-07-08 23:13:26 +0200

    add concealedArmor armorSkin

--- src/types/CosmeticProperties.ts
+++ src/types/CosmeticProperties.ts
@@ -1,5 +1,5 @@
 export type Effect = "emissive" | "combatReactive" | "killReactive" | "bossKillReactive" | "animated";
 export type ShipType = "sailsEmblem" | "sailsPattern" | "sailsColor" | "hull" | "crowsNest" | "trophy" | "wheel" | "helmDecor" | "ornaments" | "figureHead" | "nameplate"
-    | "pet" | "crewLook" | "aura" | "shipSkin";
+    | "pet" | "crewLook" | "aura" | "shipSkin" | "armorSkin";
 export type PlayerOutfitType = "headwear" | "faceAccessory" | "torso" | "neck" | "forearm" | "hand" | "waist" | "bottom" | "footwear" | "bodyCosmetics" | "emote";
 export type PlayerAppearanceType = "hair" | "facialHair" | "eyeColor";
