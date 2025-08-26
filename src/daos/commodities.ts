8ea0dfda23ef8225ce7fe466516501ff8cb83c4c
herby2212 <12448284+herby2212@users.noreply.github.com>
2025-08-26 11:51:14 +0200

    add contract to commodities dao

--- src/daos/commodities.ts
+++ src/daos/commodities.ts
@@ -1,6 +1,7 @@
 import commoditiesData from "../../data/commodities.json";
 import { CommodityCategory } from "../types/Category";
 import { Rarity } from "../types/Rarity";
+import { Contract, Contracts } from "./contracts";
 import { Event, Events } from "./events";
 import { Faction, Factions } from "./factions";
 
@@ -11,17 +12,21 @@ export class Commodity {
         public readonly category: CommodityCategory,
         public readonly event?: Event,
         public readonly faction?: Faction,
+        public readonly contract?: Contract
     ) {}
 
     public static fromRawData(rawData: any): Commodity {
         const event = rawData.event as keyof typeof Events;
         const faction = rawData.faction as keyof typeof Factions;
+        const contract = rawData.contract as keyof typeof Contracts;
+
         return new Commodity(
             rawData.id,
             rawData.rarity as Rarity,
             rawData.category as CommodityCategory,
             event ? Events[event] : undefined,
-            faction ? Factions[faction] : undefined
+            faction ? Factions[faction] : undefined,
+            contract ? Contracts[contract] : undefined
         );
     }
 
