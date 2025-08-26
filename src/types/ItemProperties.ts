ae3a00d2dc588072ba9f95ab4883298d0d4adc4a
herby2212 <12448284+herby2212@users.noreply.github.com>
2025-08-26 12:05:59 +0200

    move quest items from commodities to items

--- src/types/ItemProperties.ts
+++ src/types/ItemProperties.ts
@@ -2,7 +2,7 @@ export const Tiers = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 ] as const;
 export type Tier = (typeof Tiers)[number];
-export type GeneralType = "consumable" | "ammunition" | "armor" | "tool" | "chest";
+export type GeneralType = "consumable" | "ammunition" | "armor" | "tool" | "chest" | "quest" | "contract";
 export const WeaponTypes = [
     "culverin", "longGun", "demicannon", "bombard", "torpedo", "ballista", "seaFire", "mortar", "rocket", "springloader"
 ] as const;
