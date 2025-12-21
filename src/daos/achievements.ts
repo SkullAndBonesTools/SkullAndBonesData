7ee9e040093869825a580b42307aa9deab31aaa8
herby2212 <12448284+herby2212@users.noreply.github.com>
2025-12-21 18:01:31 +0100

    add event property to achievements data

--- src/daos/achievements.ts
+++ src/daos/achievements.ts
@@ -1,21 +1,25 @@
 import achievementData from "../../data/achievements.json";
 import { AchievementCategory } from "../types/Category";
+import { Event, Events } from "./events";
 
 export class Achievement {
     constructor(
         public readonly id: string,
         public readonly tier: number | undefined,
         public readonly category: AchievementCategory,
+        public readonly event: Event | undefined,
         public readonly goal: number,
         public readonly dateAdded: Date,
         public readonly lastUpdated: Date,
     ) {}
 
     public static fromRawData(key: string, rawData: any): Achievement {
+        const event = rawData.event as keyof typeof Events;
         return new Achievement(
             key,
             rawData.tier ?? undefined,
             rawData.category as AchievementCategory,
+            event ? Events[event] : undefined,
             rawData.goal,
             new Date(rawData.dateAdded),
             new Date(rawData.lastUpdated)
