df141bf706d59ba06b7bab8f26219e841bcbf27e
herby2212 <12448284+herby2212@users.noreply.github.com>
2026-06-30 19:52:08 +0200

    add inofficial property to sets

--- src/daos/sets.ts
+++ src/daos/sets.ts
@@ -1,20 +1,30 @@
 import setsData from '../../data/sets.json';
 import { SetType } from '../types/SetProperties';
 
+type RawSet = {
+    id: string;
+    type: SetType;
+    inofficial?: boolean;
+};
+
+const typedSetsData = setsData as Record<string, RawSet>;
+
 export class Set {
     constructor(
         public readonly id: string,
         public readonly dataType: "set",
-        public readonly type: SetType
+        public readonly type: SetType,
+        public readonly inofficial: boolean
     ) {}
 
     public static loadSets(): Record<string, Set> {
         const sets: Record<string, Set> = {};
-        for (const [key, value] of Object.entries(setsData)) {
+        for (const [key, value] of Object.entries(typedSetsData)) {
             sets[key] = new Set(
                 value.id,
                 "set",
-                value.type as SetType
+                value.type as SetType,
+                value.inofficial ?? false
             );
         }
         return sets;
