c9c45dad6882be4fb25da07ea227d6530ad18262
herby2212 <12448284+herby2212@users.noreply.github.com>
2026-07-26 18:45:16 +0200

    update mod properties

--- src/types/ModificationProperties.ts
+++ src/types/ModificationProperties.ts
@@ -1,15 +1,20 @@
 /**
  * Grade overview based on weapon rarity:
  * - Common: Basic, Basic, Advanced, Special
- * - Uncommon: Basic, Advanced, Special
- * - Rare: Basic, Advanced, Special
- * - Epic: Basic, Advanced, Advanced
+ * - Uncommon: Basic, Advanced, Special, Mythic
+ * - Rare: Basic, Advanced, Special, Mythic
+ * - Epic: Basic, Advanced, Advanced, Mythic
  */
 export const ModificationGrades = [
-    "basic", "advanced", "special"
+    "basic", "advanced", "special", "mythic"
 ] as const;
 export type Grade = (typeof ModificationGrades)[number];
-// Defines if a modification can only be applied to a regular weapon, both to a regular and repair weapon, or only to a repair weapon.
+/**
+ * Defines if a modification can only be applied to a regular weapon, both to a regular and repair weapon, or only to a repair weapon.
+ * - none: The modification can only be applied to non-repair weapons.
+ * - shared: The modification can be applied to both regular and repair weapons.
+ * - exclusive: The modification can only be applied to repair weapons.
+ */
 export type RepairAccess = "none" | "shared" | "exclusive";
 export type EffectType = "ignoreResistance" | "bonusElementalDamage" | "addElementalDamage" | "increaseDamage" | "increaseReloadSpeed" | "extraElementalDamage" | 
     "extraDamage" | "increaseProjectileSpeed" | "increaseProjectileRange" | "reduceTimeToTarget" | "increaseCritDamage" | "increaseSiegeDamage" |
